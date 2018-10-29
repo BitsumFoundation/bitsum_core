49d9c97a38bb398e638da080d3df2e457764710d
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-10-29 11:28:09 +0500

    version 1.18.10.29

--- src/version.hpp
+++ src/version.hpp
@@ -4,8 +4,8 @@
 #pragma once
 
 // defines are for Windows resource compiler
-#define bytecoin_VERSION_WINDOWS_COMMA 1, 18, 7, 20
-#define bytecoin_VERSION_STRING "1.18.7.20"
+#define bytecoin_VERSION_WINDOWS_COMMA 1, 18, 10, 29
+#define bytecoin_VERSION_STRING "1.18.10.29"
 
 #ifndef RC_INVOKED  // Windows resource compiler
 
