a6cedfc84382486d25c61f00280d12742bfb1e89
Dmitry Tarasov <dmitry.tarasov@live.com>
2020-04-01 10:28:17 +0500

    Update seeds

--- src/CryptoNoteConfig.hpp
+++ src/CryptoNoteConfig.hpp
@@ -117,8 +117,9 @@ const uint32_t P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT      = 5000;           // 5 
 const char     P2P_STAT_TRUSTED_PUB_KEY[] = "7faf1b1a49aea1f5bc50f5df7b9087b622dc9b6e5080bae33828f71a4c7b807d";
 
 const char *const SEED_NODES[] = {
-	"89.40.4.205:28080",
-	"109.235.66.242:28080",
+	"34.91.29.2:28080",
+	"185.159.129.19:28080",
+	"47.91.93.247:28080"
 };
 
 struct CheckpointData {
